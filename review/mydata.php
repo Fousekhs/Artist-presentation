5ae407fdfde933f546316f9b2c57d22e4f09278f
Jim Fousekis <dimitris.fousekis.159@gmail.com>
2022-01-08 12:53:02 +0200

    Made the review page

---
+++ review/mydata.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Data presentation</title>
+</head>
+<body>
+    <?php
+        $servername = "localhost";
+        $username = "root"; //ή όποιον χρήστη έχετε
+        $password = ""; // αλλάξτε το
+        $dbname = "personal";
+        
+        // Δημιουργία σύνδεσης
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        
+        // Έλεγχος σύνδεσης
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        //ορισμός charset της σύνδεσης ώστε να παρουσιάζονται τα ελληνικά σωστά
+        mysqli_set_charset($conn, "utf8");
+        
+        //Δημιουργία ερωτήματος
+        $sql = "INSERT INTO `supernova-review`(`name`, `release-date`, `review-by`, `comments`, `stars` VALUES
+        ('".$_POST['name']."','".$_POST['release_date']."', '".$_POST['review-by']."', '".$_POST['comments']."','".$_POST['stars']."') ;";
+        
+        //εκτέλεση ερωτήματος στη βάση
+        $result = mysqli_query($conn, $sql);
+        
+        //έλεγχος αποτελεσμάτων
+        if ($result) {
+            //Εμφάνιση αποτελεσμάτων σε μορφή πίνακα
+            echo "<br>αποθηκευση οκ<br>";
+        }
+        
+        //κλείσιμο σύνδεσης
+        mysqli_close($conn);
+    ?>
+</body>
+</html>
